878e39631c7630b28019cb6d25abe6fe6704182e
Alfaya <fcalfaya@gmail.com>
2023-03-05 15:07:23 -0300

    Update iris.sh

--- iris.sh
+++ iris.sh
@@ -4,16 +4,9 @@
 apt-get update
 apt-get upgrade -y
 
-# install wget
-apt-get install wget
-
-# install python
-wget https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz
-tar xzf Python-3.9.1.tgz
-cd Python-3.9.1
-/configure --enable-optimizations
-make -j 2
-make alt install
+apt update
+apt upgrade -y
+apt install -y python3
 
 # install Node.js
 apt-get install nodejs-legacy -y
@@ -29,6 +22,7 @@ cd iris
 
 echo "IRIS_PORT = 5000" > .env
 echo "IRIS_DB=irisDB" >> .env
+
 
 # isntall project dependencies 
 npm install
