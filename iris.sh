b339e9efe223560757468f1ef7b9f9ec8592ecef
Alfaya <fcalfaya@gmail.com>
2023-03-05 19:52:10 -0300

    Update iris.sh

--- iris.sh
+++ iris.sh
@@ -23,6 +23,8 @@ cd iris
 echo "IRIS_PORT = 5000" > .env
 echo "IRIS_DB=irisDB" >> .env
 
+mkdir images
+
 # install project dependencies 
 npm install
 
@@ -31,29 +33,3 @@ npm install sqlite3 --build-from-source --sqlite=/data/data/com.termux/files/usr
 
 # start projeto
 npm start
-
-# # Cria um serviço Systemd para o script
-# bash -c 'cat > /etc/systemd/system/iris.service <<EOF
-# [Unit]
-# Description=inicialização do servidor iris
-
-# [Service]
-# ExecStart=/data/data/com.termux/files/home/iris/start.sh
-
-# [Install]
-# WantedBy=multi-user.target
-# EOF'
-
-# # Recarrega as configurações do Systemd
-# systemctl daemon-reload
-
-# # Inicia o serviço automaticamente na inicialização
-# systemctl enable iris.service
-
-# Copy the script to /etc/init.d/ and make it executable
-cp iris.sh /etc/init.d/
-chmod +x /etc/init.d/iris.sh
-
-# Create the service symlink and enable it
-update-rc.d iris.sh defaults
-systemctl enable iris.sh
