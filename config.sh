6412a5bcd459cdd9ba92c198affbbc5e8516d770
Alfaya <fcalfaya@gmail.com>
2023-03-05 13:07:48 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 # Atualiza os pacotes do sistema
-sudo apt-get update
-sudo apt-get upgrade -y
+apt-get update
+apt-get upgrade -y
+apt update 
+apt upgrade
+apt install sudo
+
+# instala o sudo
+
 
 # Instala o Node.js
 sudo apt-get install nodejs -y
+
+# Vai para a pasta raiz
+cd ..
 
 # Baixa o repositório específico do Git
 git clone https://github.com/flavioAlfaya/iris.git iris
@@ -17,7 +26,7 @@ echo "IRIS_PORT = 5000" > .env
 echo "IRIS_DB=irisDB" >> .env
 
 # Instala as dependências do projeto
-npm install
+sudo npm install
 
 # Cria um serviço Systemd para o script
 sudo bash -c 'cat > /etc/systemd/system/iris.service <<EOF
